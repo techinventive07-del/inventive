87342e4b2a6089fb597d60315b187785a8acf2a5
prashant <prashantkkhanal.com.np>
2026-02-23 22:56:29 +0545

    wp cache added

---
+++ function.php
@@ -0,0 +1,12 @@
+<?php
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// TEMPORARY: Disable all caching for development
+define('WP_CACHE', false);
+
+
+
+define('INVENTIVE_VERSION', '1.0.0');
